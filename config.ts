f70a555fd3018cb29f8b5c58bd9fd4a42e11c421
Hau99082005 <hau99082005@gmail.com>
2025-07-11 14:41:22 +0700

    master

--- config.ts
+++ config.ts
@@ -1,4 +1,4 @@
 export const DB_URI = "mongodb+srv://hau99082005:Hau22082005@cluster0.axpylfg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-export const API = "http://localhost:3000/api"; 
+export const API = "https://javascript-2-lxxjuckay-le-van-haus-projects.vercel.app/api"; 
 export const NEXTAUTH_SECRET = "5136ce026c51aa7f37ee43d5a1f8731ef938b3a3502aa6d9901bc1f899923887";
 export const GOOGLE_API_KEY = "AIzaSyC5JCZGv0M3xzK9Sk9KEwEiwOdAMTwFg_0";
